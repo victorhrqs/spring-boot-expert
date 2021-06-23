eba18dd2b848af2874cac1043e145818775d252d
Victor Henrique <victorhrqs@gmail.com>
2021-06-23 20:13:14 -0300

    adicionado anotações restantes para validacao de entrada de dados

--- udemy/src/main/java/br/com/udemy/dto/OrderItemDTO.java
+++ udemy/src/main/java/br/com/udemy/dto/OrderItemDTO.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderItemDTO {
+
+    @NotNull(message = "product field is required")
     private Integer product;
+
+    @NotNull(message = "quantity field is required")
     private Integer quantity;
 }
