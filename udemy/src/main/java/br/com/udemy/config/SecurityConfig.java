053fdfef0d4d46f14dfc03b15e856f1ced7fc44c
Victor Henrique <victorhrqs@gmail.com>
2021-06-23 23:21:53 -0300

    Adicionando o Spring Security e configurando acesso via formLogin

---
+++ udemy/src/main/java/br/com/udemy/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package br.com.udemy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("vttr")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/orders/**")
+                    .hasRole("USER")
+                .antMatchers("/**")
+                    .hasRole("ADMIN")
+
+
+//                .authenticated()
+            .and()
+                .formLogin(); // cria o formulario de login do spring security
+//                .permitAll();
+    }
+
+
+}
