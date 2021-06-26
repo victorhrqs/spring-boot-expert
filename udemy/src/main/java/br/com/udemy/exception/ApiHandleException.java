1f8602db449f1bf0c6b58e86e227413517c7600c
Victor Henrique <victorhrqs@gmail.com>
2021-06-25 23:23:58 -0300

    Customizando exception de error

--- udemy/src/main/java/br/com/udemy/exception/ApiHandleException.java
+++ udemy/src/main/java/br/com/udemy/exception/ApiHandleException.java
@@ -2,11 +2,14 @@ package br.com.udemy.exception;
 
 import br.com.udemy.dto.ErrorDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,5 +28,16 @@ public class ApiHandleException {
     public ErrorDTO handleMethodNotValidException (MethodArgumentNotValidException ex) {
         List<String> errosList = ex.getBindingResult().getAllErrors().stream().map(e -> e.getDefaultMessage()).collect(Collectors.toList());
         return ErrorDTO.builder().erros(errosList).build();
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleUserOrPasswordInvalidException (ResponseStatusException ex) {
+
+        List<String> errorList = new ArrayList<>();
+
+        if (ex != null) errorList.add(ex.getMessage());
+
+        return ErrorDTO.builder().erros(errorList).build();
     }
 }
